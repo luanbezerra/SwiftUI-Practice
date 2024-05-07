e8d2d27c745d1f17e6fb133dab866dd244a4219c
luanbezerra <luanbezerra@protonmail.com>
2024-05-07 11:10:52 -0300

    Files creation

---
+++ caoculadora/caoculadora/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  caoculadora
+//
+//  Created by Luan Bezerra Coelho on 07/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ caoculadora/caoculadora/caoculadoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  caoculadoraApp.swift
+//  caoculadora
+//
+//  Created by Luan Bezerra Coelho on 07/05/24.
+//
+
+import SwiftUI
+
+@main
+struct caoculadoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
