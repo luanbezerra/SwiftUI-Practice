0b3e9c37afd4fe7eecf699a0eb7df17d2389a984
luanbezerra <luanbezerra@protonmail.com>
2024-05-09 11:49:25 -0300

    Tela básica do app ainda não funcional

---
+++ Closure/Closure/main.swift
@@ -0,0 +1,51 @@
+//
+//  main.swift
+//  Closure
+//
+//  Created by Luan Bezerra Coelho on 08/05/24.
+//
+
+import Foundation
+
+func addTwoInts (a: Int, b: Int) -> Int {
+    return a + b
+}
+
+//rint(addTwoInts(a: 4, b: 3))
+
+var addTwoIntys = {(a: Int, b: Int) -> Int in
+    return a + b
+}
+
+var cunty: (Int, Int) -> Int = { $0 + $1 }
+
+
+//addTwoIntys(1, 1)
+//print(cunty(1, 2))
+
+let numbers = [1, 2, 3, 4, 5]
+
+func sqrtnum(array: [Int]) -> [Int]{
+    var result: [Int] = []
+    for number in array {
+        result.append(number * number)
+    }
+    return result
+}
+
+let squaredArray = sqrtnum(array: numbers)
+
+//print(squaredArray)
+
+var sqrtint: (Int) -> Int = { a in a * a }
+
+sqrtint = {$0 * $0}
+
+let input = [1, 2, 3]
+
+let resultado = input.map(sqrtint)
+
+let teste = input.map{$0 * $0}
+
+print(resultado)
+print(teste)
--- caoculadora/caoculadora/ContentView.swift
+++ caoculadora/caoculadora/ContentView.swift
@@ -12,11 +12,16 @@ struct ContentView: View {
     @State var texto: String = ""
     @State var years: Int?
     @State var months: Int?
+    @State var result: Int?
     
     var body: some View {
+            
         VStack(alignment: .leading) {
+            
             Text("Qual a idade do seu cão?")
+                .padding()
             Text("Anos")
+                .padding()
             TextField(
                 "Quantos anos completos tem seu cão?",
                 value: $years,
@@ -25,7 +30,9 @@ struct ContentView: View {
             .textFieldStyle(.roundedBorder)
             .keyboardType(.numberPad)
             .padding()
+            
             Text("Meses")
+                .padding()
             TextField(
                 "E quantos meses além disso ele tem?",
                 value: $months,
@@ -34,6 +41,34 @@ struct ContentView: View {
             .textFieldStyle(.roundedBorder)
             .keyboardType(.numberPad)
             .padding()
+            
+            if let result {
+                Text("Seu cachorro tem, em idade humana...")
+                    .padding()
+                Text("\(result) anos")
+                    .padding()
+            } else {
+                Image(ImageResource.clarinha)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(maxHeight: 150)
+                    .frame(maxWidth: .infinity)
+                    .padding()
+            }
+            
+            Button(action: {
+                result = 23
+            })
+            {
+                Text("Calculá")
+            }
+            .padding()
+            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+            .frame(height: 50)
+            .background(Color.orange)
+            .foregroundColor(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            
         }
         .padding()
     }
