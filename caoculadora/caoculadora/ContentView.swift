a1ab0947b61903811080edaa71be69a516ff920d
luanbezerra <luanbezerra@protonmail.com>
2024-05-14 11:05:15 -0300

    Styling the screen and adding the scroll view

--- caoculadora/caoculadora/ContentView.swift
+++ caoculadora/caoculadora/ContentView.swift
@@ -22,6 +22,7 @@ struct ContentView: View {
     @State var result: Int?
     
     @State var size = Porte.small
+    @State var colorr: Color = .green
     
     var body: some View {
          
@@ -31,41 +32,70 @@ struct ContentView: View {
                     
                     Text("Qual a idade do seu cão?")
                         .font(.header5)
-                    Text("Anos")
-                        .font(.body1)
-                    TextField(
-                        "Quantos anos completos tem seu cão?",
-                        value: $years,
-                        format: .number
-                    )
+                        .foregroundColor(.indigo600)
+                        .padding(.top, 23)
+                        .padding(.bottom, 29)
                     
-                    Text("Meses")
-                        .font(.body1)
-                    TextField(
-                        "E quantos meses além disso ele tem?",
-                        value: $months,
-                        format: .number
-                    )
-                    
-                    Text("Porte")
-                        .font(.body1)
-                    
-                    Picker("Portes", selection: $size){
-                        ForEach(Porte.allCases, id:\.self) {size in
-                            Text(size.rawValue)
-                        }
+                    VStack(alignment: .leading, spacing: 8){
+                        Text("Anos")
+                            .font(.body1)
+                            .foregroundColor(.indigo600)
+                        TextField(
+                            "Quantos anos completos tem seu cão?",
+                            value: $years,
+                            format: .number
+                        )
                     }
-                    .pickerStyle(.segmented)
                     
-                    Spacer()
+//                    ZStack{
+//                        Color(colorr)
+//                        Button("Mudar de cor", action: {
+//                            colorr = .pink
+//                        })
+//                        .animation(.easeIn, value: colorr)
+//                    }
                     
+                    VStack(alignment: .leading, spacing: 8){
+                        Text("Meses")
+                            .font(.body1)
+                            .foregroundColor(.indigo600)
+                        TextField(
+                            "E quantos meses além disso ele tem?",
+                            value: $months,
+                            format: .number
+                        )
+                    }
+                    
+                    VStack(alignment: .leading, spacing: 21){
+                        Text("Porte")
+                            .font(.body1)
+                            .foregroundColor(.indigo600)
+                        
+                        Picker("Portes", selection: $size){
+                            ForEach(Porte.allCases, id:\.self) {size in
+                                Text(size.rawValue)
+                            }
+                        }
+                        .pickerStyle(.segmented)
+                    }
+                    
+//                    Divider()
+//                        .background(.indigo600)
+                                    
                     if let result {
                         Text("Seu cachorro tem, em idade humana...")
+                            .foregroundColor(.indigo600)
+                            .frame(maxWidth: .infinity)
                             .font(.body1)
+                            .padding(.bottom, 45)
+
                         Text("\(result) anos")
+                            .foregroundColor(.indigo600)
+                            .frame(maxWidth: .infinity)
                             .font(.display)
                     } else {
-                        Image(ImageResource.clarinha)
+                        Spacer()
+                        Image(ImageResource.clarinha2)
                             .resizable()
                             .scaledToFit()
                             .frame(maxHeight: 150)
@@ -78,7 +108,7 @@ struct ContentView: View {
                     Button("Calcular", action: processYears)
                     .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
                     .frame(height: 50)
-                    .background(Color.orange)
+                    .background(Color.indigo600)
                     .foregroundColor(.white)
                     .clipShape(RoundedRectangle(cornerRadius: 10))
                     
@@ -90,9 +120,17 @@ struct ContentView: View {
             }
             .navigationTitle("Cãoculadora")
             .scrollDismissesKeyboard(.immediately)
+            .toolbarBackground(.indigo600, for: .navigationBar)
+            .toolbarBackground(.visible, for: .navigationBar)
+            .toolbarColorScheme(.dark, for: .navigationBar)
         }
+        .fontDesign(.rounded)
     }
     
+}
+
+//MARK: - Functions
+extension ContentView {
     func processYears(){
         print("cãocular")
         
