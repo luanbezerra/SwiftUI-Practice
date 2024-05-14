a2cff84c03ed5fe722a3d7cd8ff795ef7271c490
luanbezerra <luanbezerra@protonmail.com>
2024-05-14 09:35:25 -0300

    Adding scroll view

--- caoculadora/caoculadora/ContentView.swift
+++ caoculadora/caoculadora/ContentView.swift
@@ -26,67 +26,70 @@ struct ContentView: View {
     var body: some View {
          
         NavigationStack {
-            VStack(alignment: .leading, spacing: 20) {
-                
-                Text("Qual a idade do seu cão?")
-                    .font(.header5)
-                Text("Anos")
-                    .font(.body1)
-                TextField(
-                    "Quantos anos completos tem seu cão?",
-                    value: $years,
-                    format: .number
-                )
-                .textFieldStyle(.roundedBorder)
-                .keyboardType(.numberPad)
-                
-                Text("Meses")
-                    .font(.body1)
-                TextField(
-                    "E quantos meses além disso ele tem?",
-                    value: $months,
-                    format: .number
-                )
-                .textFieldStyle(.roundedBorder)
-                .keyboardType(.numberPad)
-                
-                Text("Porte")
-                    .font(.body1)
-                
-                Picker("Portes", selection: $size){
-                    ForEach(Porte.allCases, id:\.self) {size in
-                        Text(size.rawValue)
-                    }
-                }
-                .pickerStyle(.segmented)
-                
-                Spacer()
-                
-                if let result {
-                    Text("Seu cachorro tem, em idade humana...")
+            ScrollView {
+                VStack(alignment: .leading, spacing: 20) {
+                    
+                    Text("Qual a idade do seu cão?")
+                        .font(.header5)
+                    Text("Anos")
                         .font(.body1)
-                    Text("\(result) anos")
-                        .font(.display)
-                } else {
-                    Image(ImageResource.clarinha)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(maxHeight: 150)
-                        .frame(maxWidth: .infinity)
-                        .shadow(radius: 5)
+                    TextField(
+                        "Quantos anos completos tem seu cão?",
+                        value: $years,
+                        format: .number
+                    )
+                    
+                    Text("Meses")
+                        .font(.body1)
+                    TextField(
+                        "E quantos meses além disso ele tem?",
+                        value: $months,
+                        format: .number
+                    )
+                    
+                    Text("Porte")
+                        .font(.body1)
+                    
+                    Picker("Portes", selection: $size){
+                        ForEach(Porte.allCases, id:\.self) {size in
+                            Text(size.rawValue)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                    
+                    Spacer()
+                    
+                    if let result {
+                        Text("Seu cachorro tem, em idade humana...")
+                            .font(.body1)
+                        Text("\(result) anos")
+                            .font(.display)
+                    } else {
+                        Image(ImageResource.clarinha)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(maxHeight: 150)
+                            .frame(maxWidth: .infinity)
+                            .shadow(radius: 5)
+                    }
+                    
+                    Spacer()
+                    
+                    Button("Calcular", action: processYears)
+                    .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                    .frame(height: 50)
+                    .background(Color.orange)
+                    .foregroundColor(.white)
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    
                 }
-                
-                Spacer()
-                
-                Button("Calcular", action: processYears)
-                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
-                .frame(height: 50)
-                .background(Color.orange)
-                .foregroundColor(.white)
-                .clipShape(RoundedRectangle(cornerRadius: 10))
-                
+                .textFieldStyle(.roundedBorder)
+                .keyboardType(.numberPad)
+                .padding()
+            .containerRelativeFrame(.vertical)
             }
-            .padding()
+            .navigationTitle("Cãoculadora")
+            .scrollDismissesKeyboard(.immediately)
         }
     }
     
